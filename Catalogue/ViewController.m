d5fd497de81142c641ceafc65c559ab27c40c2f7
lsujstans7 <jstans7@lsu.edu>
2012-08-03 13:42:02 -0500

    This is the example from class.  We started new after lunch.

---
+++ Catalogue/ViewController.m
@@ -0,0 +1,91 @@
+//
+//  ViewController.m
+//  Catalogue
+//
+//  Created by kcfancher on 8/3/12.
+//  Copyright (c) 2012 kcfancher. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "Book.h"
+#import "CD.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+@synthesize itemsTableView;
+@synthesize items = _items;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	    
+    self.items = [NSMutableArray array];
+        
+    [self.items addObject:[Book bookWithTitle:@"Objective-C" author:@"Keli" price:1.99 identificationNumber:100]];
+    [self.items addObject:[Book bookWithTitle:@"Objective-C 2.0" author:@"Keli" price:1.99 identificationNumber:100]];
+
+}
+
+- (void)viewDidUnload
+{
+    [self setItemsTableView:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+// Here are the two required UITableViewDataSource Methods
+
+// Every TableView can have a number of different sections
+// An example is the letter heading in the Contacts application
+// This method returns the number of rows that are in each section
+// For an app with no sections this method just returns the total number of rows
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // We want there to be one row for each item in our items array
+    return self.items.count;
+}
+
+// tableView:cellForRowAtIndexPath returns a UITableViewCell that has been customized
+// for each tableViewCell in our TableView (This method is called once per row/cell in our tableview)
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // We first have to create a UITableViewCell instance
+    // We use UITableView's insntance method -(UITableViewCell *)dequeueReusableCellWithIdentifier to
+    // Return a TableViewCell that is memory efficient
+    // We pass it the same string that we have used as an identifier in our Interface
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ItemCell"];
+    
+    // First we have to get the Item for the current row
+    // IndexPath is an object that has both a section and a row property
+    // Our catalogue stores both Book's and CD's - but since both are subclasses
+    // of Item we can declare it universally as a Item
+    Book *bookInstance = [self.items objectAtIndex:indexPath.row];
+    
+    
+    // Set the value of our cell's textLabel
+    cell.textLabel.text = bookInstance.title;
+    
+    return cell;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+@end
